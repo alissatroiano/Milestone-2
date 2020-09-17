d279889101c3d13856768a43da9e8aab5bf71620
developer <developer@t3chsavages-MBP.fios-router.home>
2020-09-16 23:18:31 -0400

    Write new spec and refactor until test passes

--- assets/spec/questionSpec.js
+++ assets/spec/questionSpec.js
@@ -1,8 +1,12 @@
-describe("Quiz", function () {
-    describe('Quiz test suite', function () {
-        it('Quiz test', function () {
+describe("Quiz Test", function () {
+    describe('Quiz()', function () {
+        it('Should accept the questions object as a parameter', function () {
             expect(Quiz()).toBe(questions);
         });
     });
-
+    describe('Quiz()', function () {
+        it('Should generate a score when called with showScores', function () {
+            expect(Quiz()).toBe(quiz.scores);
+        });
+    });
 });

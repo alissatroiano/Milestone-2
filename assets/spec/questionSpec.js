e29cf59304ba42a0d42f48e1135ee8324e6e709d
developer <developer@t3chsavages-MBP.fios-router.home>
2020-09-17 00:06:47 -0400

    Spot a bug after creating new spec

--- assets/spec/questionSpec.js
+++ assets/spec/questionSpec.js
@@ -5,7 +5,7 @@ describe("Game Of Facts Test Suite", function () {
         });
     });
     describe('Quiz()', function () {
-        it('Should use this to create quiz.score ', function () {
+        it('Should use this to create quiz.score', function () {
             expect(Quiz()).toBe(quiz.scores);
         });
     });
@@ -19,5 +19,34 @@ describe("Game Of Facts Test Suite", function () {
             expect(Question()).toBe(Question.text);
         });
     });
-
+    describe('Question()', function () {
+        it('Should use this keyword to create choices object', function () {
+            expect(Question()).toBe(Question.choices);
+        });
+    });
+    describe('Question()', function () {
+        it('Should use this keyword to create answer object', function () {
+            expect(Question()).toBe(Question.answer);
+        });
+    });
+    describe('Question()', function () {
+        it('Should use this keyword to create description object', function () {
+            expect(Question()).toBe(Question.description);
+        });
+    });
+    describe('Question()', function () {
+        it('Should use this keyword to create image object', function () {
+            expect(Question()).toBe(Question.image);
+        });
+    });
+    describe('Question()', function () {
+        it('Should use this keyword to create footer object', function () {
+            expect(Question()).toBe(Question.footer);
+        });
+    });
+    describe('play()', function() {
+        it('Should show scores if quiz has ended', function() {
+            expect(quiz.isEnded()).toBe(showScores(gameOverHTML))
+        })
+    })
 });
